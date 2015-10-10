f486977fd2fed668cae659a96bf3fb673a2abfdf
Felipe Moraes <felipemoraes@Felipes-MacBook-Air.local>
2015-10-10 13:10:40 -0300

    Implemented greedy algorithm

--- src/solve_coin_change_ag.cc
+++ src/solve_coin_change_ag.cc
@@ -14,4 +14,44 @@ SolveCoinChangeAg::SolveCoinChangeAg(string changes_file, string coins_file, str
 
 
 void SolveCoinChangeAg::solve(){
+
+    std::vector<int> solution;
+    std::string line;   
+    ifstream fchange(changes_file_);
+    ofstream foutput(output_file_);
+    for (int i = 0; i < coins_->size(); ++i){
+        solution.push_back(0);
+    }
+    std::sort(coins_->begin(),coins_->end(), std::greater<int>());
+    if (fchange.is_open()){
+        while (getline(fchange,line)){
+            int change = stoi(line);
+            int best_fewest_coins = 0;
+       
+            for (int i = 0; i<coins_->size();++i){
+                while (change>= 0){
+                    change -= (*coins_)[i]; 
+                    if (change >=0){
+                        solution[i]++;
+                        best_fewest_coins++;
+                    }
+                }
+                if (change < 0){
+                    change += (*coins_)[i];
+                }
+            }
+            foutput << best_fewest_coins << " ";
+            for (int i = 0; i<solution.size() ;++i){
+                if (solution[i] != 0) {
+                    while (solution[i]>0){
+                        foutput << (*coins_)[i] << " ";
+                        solution[i]--;
+                    }
+                }     
+            }         
+            foutput << std::endl;        
+       }
+    } else {
+        std::cout << "File not found: " << changes_file_ << std::endl;
+    }
 }

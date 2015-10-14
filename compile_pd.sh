6d2192b9d2b1410888c5a56beeb86ab398d751d2
Felipe Moraes <felipemoraes@flash.adm.se.latin.dcc.ufmg.br>
2015-10-14 20:57:12 -0300

    C++11 flag and -O3

--- compile_pd.sh
+++ compile_pd.sh
@@ -1,2 +1,2 @@
 mkdir -p bin
-g++ -O3 -o bin/Assassin_sGreedV src/main.cc src/solve_coin_change.cc src/solve_coin_change_fb.cc src/solve_coin_change_pd.cc src/solve_coin_change_ag.cc
+g++ -std=c++11 -O3 -o bin/Assassin_sGreedV src/main.cc src/solve_coin_change.cc src/solve_coin_change_fb.cc src/solve_coin_change_pd.cc src/solve_coin_change_ag.cc
